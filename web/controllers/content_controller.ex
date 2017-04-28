1911d8bc2468bb61fc424f6351e1f7dae3a04b38
Itay Maoz <itay@soluto.com>
2017-04-29 00:12:58 +0300

    properly handle errors in read operation

--- web/controllers/content_controller.ex
+++ web/controllers/content_controller.ex
@@ -5,8 +5,10 @@ defmodule Gittp.ContentController do
   def read(conn, %{"path" => path}) do
     repo_path = Application.get_env(:gittp, Gittp.Endpoint)[:local_repo_path]
     repo = Git.new repo_path
-    {_, content} = Gittp.Repo.content(repo, path)
-    json conn, content
+    case Gittp.Repo.content(repo, path) do
+      {:ok, content} -> json conn, content
+      {:error, error} -> handle_error(conn, error)
+    end
   end
 
   def write(conn, %{"path" => _, "content" => _, "checksum" => _, "commit_message" => _} = params) do
@@ -30,8 +32,11 @@ defmodule Gittp.ContentController do
   defp handle_error(conn, error) do 
     case error do
       :checksum_mismatch -> send_resp(conn, 400, "Checksum mismatch, please read the file again")
-      :file_exists -> send_resp(conn, 400, "File alredy exists")      
-      error -> send_resp(conn, 500, inspect error)
+      :file_exists -> send_resp(conn, 400, "File alredy exists")   
+      :enoent -> send_resp(conn, 400, "File does not exist")   
+      error -> 
+        Logger.error inspect error
+        send_resp(conn, 500, "Unknown error")
     end
   end 
 end
