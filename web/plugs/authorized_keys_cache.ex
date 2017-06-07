1612a3167534bca6623cd4ce159553a2182a99d1
Itay Maoz <itay@soluto.com>
2017-06-07 16:18:49 +0300

    fix a bug in authorized keys cache

--- web/plugs/authorized_keys_cache.ex
+++ web/plugs/authorized_keys_cache.ex
@@ -3,7 +3,7 @@ defmodule Gittp.Web.Plugs.AuthorizedKeysCache do
         authorized_keys = 
             System.get_env("API_KEYS_FILE_PATH")
             |> File.stream!([:read, :utf8])
-            |> Enum.reduce([], fn (current, list) -> [current | list] end)
+            |> Enum.reduce([], fn (current, list) -> [String.trim(current) | list] end)
 
         Agent.start_link(fn -> authorized_keys end, [name: __MODULE__])
     end
