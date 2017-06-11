2e06448e123106ee67d3a45316e5cd9d3d5954bb
Yshayy <yshayy@gmail.com>
2017-06-11 16:01:10 +0300

    Create start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ -n "$SSH_PRIVATE_KEY" ]; then
     mkdir ~/.ssh/
-    ssh-keyscan $REMOTE_GIT_HOST >> /tmp/hosts
+    ssh-keyscan -p $REMOTE_GIT_PORT $REMOTE_GIT_HOST  >> /tmp/hosts
     ssh-keygen -lf /tmp/hosts >> ~/.ssh/known_hosts
     rm /tmp/hosts
     cp $SSH_PRIVATE_KEY /tmp/key
